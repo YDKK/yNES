57ed4a590b3f14f4d6697c8d7a5e37491ec3a806
YDKK <YDKK@users.noreply.github.com>
2022-10-24 22:23:41 +0900

    とりあえずAudioWorkletProcessorで実装してみた

--- src/wasm/audio-processor.js
+++ src/wasm/audio-processor.js
@@ -1,4 +1,5 @@
-const audioBuffer = new ArrayBuffer(audio_buffer_length * 4 * 3);
+const audio_buffer_length = 3025;
+const audioBuffer = new ArrayBuffer(audio_buffer_length * 4 * 10);
 const audioBufferFloat32 = new Float32Array(audioBuffer);
 let audioBufferFilled = 0;
 let audioBufferConsumed = 0;
@@ -7,29 +8,33 @@ class AudioProcessor extends AudioWorkletProcessor {
     constructor(...args) {
         super(...args);
         this.port.onmessage = (e) => {
-            console.log(e.data);
             for (let i = 0; i < e.data.pcm_filled; i++) {
                 audioBufferFloat32[audioBufferFilled] = e.data.audioBufferFloat32[i];
                 audioBufferFilled++;
                 audioBufferFilled %= audioBufferFloat32.length;
                 if (audioBufferFilled == audioBufferConsumed) {
                     console.warn("buffer overrun");
-                    audioBufferConsumed++;
                 }
             }
-            this.port.postMessage("pong");
+            let diff = audioBufferFilled - audioBufferConsumed;
+            let current = diff >= 0 ? diff : audioBufferFloat32.length - diff;
+            this.port.postMessage(current);
         };
     }
     process(inputs, outputs, parameters) {
-        for (let i = 0; i < outputs.length; i++) {
-            outputs[i] = audioBufferFilled[audioBufferConsumed];
-            audioBufferConsumed++;
-            audioBufferConsumed %= audioBufferFloat32.length;
+        const output = outputs[0];
+        for (let i = 0; i < output[0].length; i++) {
             if (audioBufferFilled == audioBufferConsumed) {
                 console.warn("buffer underrun");
-                audioBufferFilled++;
+                break;
             }
-    }
+            output[0][i] = audioBufferFloat32[audioBufferConsumed];
+            audioBufferConsumed++;
+            audioBufferConsumed %= audioBufferFloat32.length;
+        }
+        let diff = audioBufferFilled - audioBufferConsumed;
+        let current = diff >= 0 ? diff : audioBufferFloat32.length - diff;
+        this.port.postMessage(current);
         return true;
     }
 }
